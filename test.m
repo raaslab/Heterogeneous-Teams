cdd4fbb6e36e77534bfcaa56214f962b6897e07d
klyu-captainamerica <klyu@vt.edu>
2016-12-15 14:55:44 -0500

    added output cluster graph

--- test.m
+++ test.m
@@ -34,7 +34,7 @@ numPoints = numel(h.XData);
 [G2, x2, y2] = graphMakingWPoints(h.XData, h.YData);
 % [S1, T1, weights] = makingSTW(numPointsInit, numBatteryLevels);
 [S1, T1, weights] = makingSTW(x2, y2, z3d, numPointsInit, numBatteryLevels);
-% graphingCluster(x3d, y3d, z3d, numPointsInit, numBatteryLevels, S1, T1);       % graph in cluster format
+graphingCluster(x3d, y3d, z3d, numPointsInit, numBatteryLevels, S1, T1);       % graph in cluster format
 
 [G2] = createEdgesFull(G2, numPoints);
 % [G2] = createEdges(G2, S1, T1, weights);
@@ -54,6 +54,7 @@ plot(G3, 'XData', x3, 'YData', y3)
 [edgeArray, S2, T2] = createEdgeArray(x_reshape, numPointsInit, numBatteryLevels);
 [G3] = createEdges(G3, S2, T2, G_final.Edges.Weight);
 plot(G3, 'XData', x3, 'YData', y3, 'EdgeLabel', G_final.Edges.Weight)
+graphingCluster(x3d, y3d, z3d, numPointsInit, numBatteryLevels, S2, T2);        % graphing the cluster format of the solution
 
 toc
 
