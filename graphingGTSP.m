a0524c76be2ba63358bbea25f8aa4e55357de65b
klyu-captainamerica <klyu@vt.edu>
2016-12-15 14:15:19 -0500

    changed name of testRandom.m, finished graphingGTSP.m

--- graphingGTSP.m
+++ graphingGTSP.m
@@ -1,18 +1,22 @@
 % graphingGTSP
 % graphs the 3D GTSP problem on a 2D plane with the cluster grouping
 % INPUTS
-
+% x = x corrdinates
+% y = y corrdinates
+% z = z corrdinates
+% numPoints = number of initial points in the graph
+% numLevels = number of initial level given for the graph
+% s = starting node for an edge
+% t = corresponding ending node for an edge with "s"
 % OUTPUTS
+% this function just plots the cluster so no output is needed
 
-% Use function "boundary" to encircle points.
-
-function [G, xOut, yOut] = graphingGTSP(x, y, z, numPoints, numLevels)
+function graphingGTSP(x, y, z, numPoints, numLevels, s, t)
 
 xOut = [];
 yOut = [];
 
 G = digraph;
-% yOut = z;
 for i = 1:numPoints
     count = numLevels;
     for j = 1:numLevels
@@ -22,11 +26,22 @@ for i = 1:numPoints
     end
 end
 
+
 [G, xOut, yOut] = graphMakingWPoints(xOut, yOut, G);
+[G] = createEdges(G, s, t)
+
+figure()                                                            % plots graph
+plot(G, 'XData', xOut, 'YData', yOut, 'LineStyle', '-.');
 
 
-
-
+yRec = 0;
+wRec = 1;
+hRec = numLevels+1;
+for i = 1:numPoints                                                 % clusters the points
+    xRec = i-(wRec/2);                                              % parameters for creating clusters
+    pos = [xRec, yRec, wRec, hRec];                                 % creates oval, but needs to be fixed
+    rectangle('Position', pos, 'Curvature', [1 1])
+end
 
 
 end
---
+++ sandBox.m
@@ -0,0 +1,17 @@
+% testRandom
+% used to test random functions
+% this is not the actual code
+% DON'T ADD CLEAR OR CLC, this way you can use existing data
+
+graphingGTSP(x3d, y3d, z3d, numPointsInit, numBatteryLevels, S1, T1);
+
+
+
+% pos = [0.75, 0, 0.5, (numBatteryLevels+1)];       % creates oval, but needs to be fixed
+% rectangle('Position', pos, 'Curvature', [1 1])
+% pos = [1.75, 0, 0.5, (numBatteryLevels+1)];
+% rectangle('Position', pos, 'Curvature', [1 1])
+% 
+% k = boundary(x', y');
+% hold on;
+% plot(x(k), y(k));
