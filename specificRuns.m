60fc87b72df1340d76be879be6a1a691e26d5c06
Kevin Li Yu <klyu@vt.edu>
2017-02-02 18:26:34 -0500

    updated

--- specificRuns.m
+++ specificRuns.m
@@ -4,10 +4,10 @@
 
 
 
-i = 8;
+i = 20;
 j = 4;
 k = 5;
-location =  77;
+location =  89;
 filename = [num2str(i) '_' num2str(j) '_' num2str(k)];
 [ansTime, problem] = test(i, j, filename);
 time(location, :) = [double(i), double(j), double(k), ansTime, double(problem)];
