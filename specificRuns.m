1a9c3573b9c063826fc6950ed71fe6de963acfd0
Kevin Li Yu <klyu@vt.edu>
2017-02-26 20:51:27 -0500

    final runs

--- specificRuns.m
+++ specificRuns.m
@@ -4,16 +4,16 @@
 
 
 
-i = 12;
-j = 4;
+i = 20;
+j = 10;
 % k = 1;
-tTO = 4;
-tL = 4;
+tTO = 0;
+tL = 0;
 rRate = 0;
 UGVS = 1;
 % location =  18;
 % filename = [num2str(i) '_' num2str(j) '_' num2str(k)];
-filename = ['kentland'];
+filename = ['20_10_0_0_0_1'];
 [ansTime, problem1, problem2] = testGeneral(i, j, filename, tTO, tL, rRate, UGVS, G, x, y);
 % time(location, :) = [double(i), double(j), double(k), ansTime, double(problem)];
 % time = [double(i), double(j), double(k), ansTime, double(problem)];
