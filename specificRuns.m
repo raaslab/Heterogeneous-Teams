f438ff137d358f3b4a1f0a6cc381f5e6f3de332f
Kevin Li Yu <klyu@vt.edu>
2017-09-11 09:26:55 -0400

    made a better createGTSPFile.m

--- specificRuns.m
+++ specificRuns.m
@@ -16,31 +16,35 @@
 % location =  975;
 % filename = [num2str(i) '_' num2str(j) '_' num2str(k)];
 % filename = ['GNLS'];
-allTimes = [];
-% for z = 11
-% z=11;
-    G = 0;
-    x = 0;
-    y = 0;
-    i = 100;         % number of sites
-    j = 100;          % number of battery levels
-    k = 1;
-    tTO = 4;        % take off cost
-    tL = 4;         % landing cost
-    rRate = 0;      % rate of recharge
-    UGVS = 4;       % time to travel one unit for the UGV
-    method = 1;     % 1 = GLNS, 0 = concorde
-%     filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(z)];
-%     filename = [num2str(i) '_' num2str(j) 'GNLS'];
-    filename = ['test40_40'];
-    [ansTime,gtspMatrix,gtspTime] = testGeneral(i, j, filename, tTO, tL, rRate, UGVS, G, x, y, method);
-    roundedGtspMatrix = round(gtspMatrix);
-    roundedGtspMatrix(roundedGtspMatrix == -1) = 999999;
+% ----------------------
+% allTimes = [];
+% % for z = 11
+% % z=11;
+% G = 0;
+% x = 0;
+% y = 0;
+% i = 100;         % number of sites
+% j = 100;          % number of battery levels
+% k = 1;
+% tTO = 4;        % take off cost
+% tL = 4;         % landing cost
+% rRate = 0;      % rate of recharge
+% UGVS = 4;       % time to travel one unit for the UGV
+% method = 1;     % 1 = GLNS, 0 = concorde
+% %     filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(z)];
+% %     filename = [num2str(i) '_' num2str(j) 'GNLS'];
+% filename = ['test40_40'];
+% [ansTime,gtspMatrix,gtspTime] = testGeneral(i, j, filename, tTO, tL, rRate, UGVS, G, x, y, method);
+% roundedGtspMatrix = round(gtspMatrix);
+% roundedGtspMatrix(roundedGtspMatrix == -1) = 999999;
+% ---------------------------
 %     filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(z) 'more'];
 %     filename = [num2str(i) '_' num2str(j) 'GNLSmore'];
-    filename = ['test40_41'];
-    f = fullfile('/home/klyu/lab/Heterogeneous-Teams/ICRA2018', filename);
-    save(f);
+filename = ['test.gtsp'];
+createGTSPFile(filename,roundedGtspMatrix, i, j)
+% filename = ['test'];
+% f = fullfile('/home/klyu/lab/Heterogeneous-Teams/ICRA2018', filename);
+% save(f);
 %     filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(z) '.gtsp'];
 %     filename = [num2str(i) '_' num2str(j) 'GNLS.gtsp'];
 %     createGTSPFile(filename,roundedGtspMatrix)
