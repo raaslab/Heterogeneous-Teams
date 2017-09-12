bf57b47d5236a1611e0f154f8a47794ca2e16941
ashish-kb <ashishkb@vt.edu>
2017-09-12 15:43:11 -0400

    updated finishing up graph for UAV for kentland flight

--- GLNSplotter.m
+++ GLNSplotter.m
@@ -1,6 +1,6 @@
 
-GLNSSolution = [2300,1585,4581,2064,2161,4354,1450,1641,4438,3432,3623,2418,613,3211,1202,1896,4189,3584,79,4972,968,3053,1141,1338,2332,4724,1720,2612,4202,897,195,3386,4674,4073,468,361,3946,2744,232,3122,1918,4810,507,2948,1039,736,2829,3818,2503,3701];
-GLNSSolution = fliplr(GLNSSolution);
+%GLNSSolution = [2300,1585,4581,2064,2161,4354,1450,1641,4438,3432,3623,2418,613,3211,1202,1896,4189,3584,79,4972,968,3053,1141,1338,2332,4724,1720,2612,4202,897,195,3386,4674,4073,468,361,3946,2744,232,3122,1918,4810,507,2948,1039,736,2829,3818,2503,3701];
+%GLNSSolution = fliplr(GLNSSolution);
 GLNSSolutionOriginalPoints = ceil(GLNSSolution./numBatteryLevels);
 
 GLNSx = zeros(1,numel(x1));
@@ -11,14 +11,14 @@ for a = 1:numPointsInit
     GLNSy(a) = y1(GLNSSolutionOriginalPoints(a));
 end
 
-GLNSx(end+1) = 0;
-GLNSy(end+1) = 0;
+% GLNSx(end+1) = 0;
+% GLNSy(end+1) = 0;
 GLNSg = digraph;
 GLNSg = addnode(GLNSg, numel(GLNSx));
 
 S2 = zeros(1,numel(GLNSx)-1);
 T2 = S2;
-for a = 1:numPointsInit
+for a = 1:numPointsInit-1
    S2(a) = a;
    T2(a) = a+1;
 end
