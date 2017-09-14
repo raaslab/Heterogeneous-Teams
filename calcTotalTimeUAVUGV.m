24842ac8395bf5f6d657738f1968234815c572e1
klyu_alien_windows <klyu@vt.edu>
2017-09-13 21:28:48 -0400

    worked on seeing if GLNS and concorde are the same for the first instances

--- calcTotalTimeUAVUGV.m
+++ calcTotalTimeUAVUGV.m
@@ -1,12 +1,21 @@
 
 
 
-function [sumTimes] = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix)
-sumTimes = 0;
-indexOfUsedPoints = find(finalMatrix==1);
-for i = 1:numel(indexOfUsedPoints)
-    timeCost = roundedGtspMatrix(indexOfUsedPoints(i));
-    sumTimes = sumTimes + timeCost;
-end
-end
+function [sumTimes] = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix, edgeWeights, method)
 
+switch method
+    case 0
+        sumTimes = 0;
+        indexOfUsedPoints = find(finalMatrix==1);
+        for i = 1:numel(indexOfUsedPoints)
+            timeCost = roundedGtspMatrix(indexOfUsedPoints(i));
+            sumTimes = sumTimes + timeCost;
+        end
+        
+    case 1
+        temp = cell2mat(edgeWeights(:,3));
+        sumTimes = sum(temp);
+        
+
+end
+end
--- totalCosts.m
+++ totalCosts.m
@@ -4,34 +4,35 @@ clear all;
 sumTimes = [];
 % for temp = 3
 %     for z = 11
-    G = 0;
-    x = 0;
-    y = 0;
-    i = 20;
-    j = 10;
-    k = 1;
-    tTO = 4;
-    tL = 4;
-    rRate = 0;
-    UGVS = 1;
-dname='/home/klyu/lab/Heterogeneous-Teams/MRS';
+G = 0;
+x = 0;
+y = 0;
+i = 20;
+j = 10;
+k = 1;
+tTO = 4;
+tL = 4;
+rRate = 0;
+UGVS = 1;
+dname='C:\Users\klyu\Documents\lab\Heterogeneous-Teams\MRS';
 filename = [num2str(i) '_' num2str(j) 'GNLS.mat'];
 fname=fullfile(dname,filename);
 load(fname);
-    G = 0;
-    x = 0;
-    y = 0;
-    i = 20;
-    j = 10;
-    k = 1;
-    tTO = 4;
-    tL = 4;
-    rRate = 0;
-    UGVS = 1;
+G = 0;
+x = 0;
+y = 0;
+i = 20;
+j = 10;
+k = 1;
+tTO = 4;
+tL = 4;
+rRate = 0;
+UGVS = 1;
+method = 1;
 filename = [num2str(i) '_' num2str(j) 'GNLSmore.mat'];
 fname=fullfile(dname,filename);
 load(fname);
-sumTimes(end+1) = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix)
+sumTimes(end+1) = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix, edgeWeightsFinal, method)
 %     end
 % end
 
