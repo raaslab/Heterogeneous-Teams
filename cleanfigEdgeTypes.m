d36453f7a6ffdbe960bb3642b260492012d596b0
klyu_alien_windows <klyu@vt.edu>
2017-09-14 09:56:46 -0400

    all cleanfig___.m files are to add text to plots/cleaned up figures/moved one file

---
+++ cleanfigEdgeTypes.m
@@ -0,0 +1,18 @@
+open EdgeTypes.fig
+% axis equal;
+% xlim([-75,125])
+
+ca = gca;
+
+% ca.Children.LineWidth = 2;
+% ca.Children.ArrowSize = 10;
+% ca.Children.MarkerSize = 6;
+
+% ca.Children.NodeLabel = '';
+for i = 1 : 1 : length(ca.Children(end).XData)-1
+    %     textOnGraph = (100/5) * ca.Children(end).YData(i);
+    textOnGraph = ['x_' num2str(ca.Children(end).XData(i)) '^' num2str(ca.Children(end).YData(i))]
+    text(ca.Children(end).XData(i) - .15, ca.Children(end).YData(i) + .35, textOnGraph, 'FontSize', 14)
+end
+% text(ca.Children(end).XData(1) + 1.5, ca.Children(end).YData(1) - 1.5, num2str(1), 'FontSize', 14)
+% text(ca.Children(end).XData(end) + 1.5, ca.Children(end).YData(end) - 1.5, num2str(length(ca.Children(end).YData)), 'FontSize', 14)
