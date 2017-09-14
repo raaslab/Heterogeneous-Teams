68c94e7291e8a608a89e821c5f1cbc5b9bcb7ad6
Kevin Li Yu <klyu@vt.edu>
2017-09-14 00:15:07 -0400

    added new picture

--- totalCosts.m
+++ totalCosts.m
@@ -1,52 +1,21 @@
 % totalCosts
 % gets the total time cost of multiple runs
 clear all;
-sumTimes = [];
+% sumTimes = [];
 % for temp = 3
-%     for z = 11
-G = 0;
-x = 0;
-y = 0;
-i = 20;
-j = 10;
+i = 18;
+j = 4;
 k = 1;
-tTO = 4;
-tL = 4;
-rRate = 0;
-UGVS = 1;
-dname='C:\Users\klyu\Documents\lab\Heterogeneous-Teams\MRS';
-filename = [num2str(i) '_' num2str(j) 'GNLS.mat'];
-G = 0;
-x = 0;
-y = 0;
-i = 20;
-j = 10;
-k = 1;
-tTO = 4;
-tL = 4;
-rRate = 0;
-UGVS = 1;
-dname='C:\Users\klyu\Documents\lab\Heterogeneous-Teams\MRS';
-filename = [num2str(i) '_' num2str(j) 'GNLS.mat'];
+disp(k)
+dname='/home/klyu/lab/heterogenous_teams/Heterogeneous-Teams/MRS';
+filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(k) '.mat'];
 fname=fullfile(dname,filename);
 load(fname);
-G = 0;
-x = 0;
-y = 0;
-i = 20;
-j = 10;
+i = 18;
+j = 4;
 k = 1;
-tTO = 4;
-tL = 4;
-rRate = 0;
-UGVS = 1;
 method = 1;
-filename = [num2str(i) '_' num2str(j) 'GNLSmore.mat'];
+filename = [num2str(i) '_' num2str(j) 'GNLS' num2str(k) 'more.mat'];
 fname=fullfile(dname,filename);
 load(fname);
-sumTimes(end+1) = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix, edgeWeightsFinal, method)
-%     end
-% end
-
-
-
+sumTimes = calcTotalTimeUAVUGV(finalMatrix, roundedGtspMatrix, edgeWeightsFinal, method)
