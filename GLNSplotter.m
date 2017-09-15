430efceef93ddb08375edd77ecb1584755a2255e
Kevin Li Yu <klyu@vt.edu>
2017-09-15 18:54:17 -0400

    added more figures/use noWhiteSpace.m

--- GLNSplotter.m
+++ GLNSplotter.m
@@ -1,14 +1,20 @@
 
-%GLNSSolution = [2300,1585,4581,2064,2161,4354,1450,1641,4438,3432,3623,2418,613,3211,1202,1896,4189,3584,79,4972,968,3053,1141,1338,2332,4724,1720,2612,4202,897,195,3386,4674,4073,468,361,3946,2744,232,3122,1918,4810,507,2948,1039,736,2829,3818,2503,3701];
-%GLNSSolution = fliplr(GLNSSolution);
+GLNSSolution = [1550, 2194, 1590, 1381, 628, 1276, 1819, 1868, 16, 313, 660, 405, 298, 896, 1991, 1788, 2082, 576, 1424, 171, 1720, 1918, 66, 710, 909, 952, 2150, 1499, 1147, 1695, 1342, 1189, 135, 2379, 2278, 2320, 2463, 1008, 206, 2202, 800, 1647, 1094, 491, 535, 828, 367, 1214, 2011, 2401, 2501];
+
+
+GLNSSolution = fliplr(GLNSSolution);
 GLNSSolutionOriginalPoints = ceil(GLNSSolution./numBatteryLevels);
 
 GLNSx = zeros(1,numel(x1));
 GLNSy = zeros(1,numel(y1));
 
 for a = 1:numPointsInit
-    GLNSx(a) = x1(GLNSSolutionOriginalPoints(a));
-    GLNSy(a) = y1(GLNSSolutionOriginalPoints(a));
+    if GLNSSolutionOriginalPoints(a) > numel(x1)
+        
+    else
+        GLNSx(a) = x1(GLNSSolutionOriginalPoints(a));
+        GLNSy(a) = y1(GLNSSolutionOriginalPoints(a));
+    end
 end
 
 % GLNSx(end+1) = 0;
@@ -19,8 +25,8 @@ GLNSg = addnode(GLNSg, numel(GLNSx));
 S2 = zeros(1,numel(GLNSx)-1);
 T2 = S2;
 for a = 1:numPointsInit-1
-   S2(a) = a;
-   T2(a) = a+1;
+    S2(a) = a;
+    T2(a) = a+1;
 end
 
 GLNSg = addedge(GLNSg,S2,T2);
@@ -33,18 +39,22 @@ T4 = S3;
 T5 = S3;
 
 for a = 1:numPointsInit-1
-    typeChecker = v_Type(GLNSSolution(a),GLNSSolution(a+1));
-    if typeChecker == 1
-        S3(end+1) = a;
-        T3(end+1) = a+1;
-    elseif typeChecker == 2
-        S4(end+1) = a;
-        T4(end+1) = a+1;
-    elseif typeChecker == 3
-        S5(end+1) = a;
-        T5(end+1) = a+1;
+    if GLNSSolutionOriginalPoints(a) > numel(x1)
+        
     else
-        disp('error')
+        typeChecker = v_Type(GLNSSolution(a),GLNSSolution(a+1));
+        if typeChecker == 1
+            S3(end+1) = a;
+            T3(end+1) = a+1;
+        elseif typeChecker == 2
+            S4(end+1) = a;
+            T4(end+1) = a+1;
+        elseif typeChecker == 3
+            S5(end+1) = a;
+            T5(end+1) = a+1;
+        else
+            disp('error')
+        end
     end
 end
 
@@ -56,21 +66,21 @@ for a = 1:numel(S4)-1                 %creating UGV edges
 end
 
 GLNSg = addedge(GLNSg,S6,T6);
-GLNSPlot = plot(GLNSg,'XData',GLNSx,'YData',GLNSy, 'LineWidth',4);
+% GLNSPlot = plot(GLNSg,'XData',GLNSx,'YData',GLNSy, 'LineWidth',4);
 
 
 
-hold on
+% hold on
 % highlight edges for UAV
-if isempty(S4) == 0                 %highlight type 2 edges
-    highlight(GLNSPlot,S4, T4,'EdgeColor','r','LineWidth',4, 'LineStyle', '--')
-end
-if isempty(S5) == 0                 %highlight type 3 edges
-    highlight(GLNSPlot,S5, T5,'EdgeColor','r','LineWidth',4, 'LineStyle', '--')
-end
+% if isempty(S4) == 0                 %highlight type 2 edges
+%     highlight(GLNSPlot,S4, T4,'EdgeColor','r','LineWidth',4, 'LineStyle', '--')
+% end
+% if isempty(S5) == 0                 %highlight type 3 edges
+%     highlight(GLNSPlot,S5, T5,'EdgeColor','r','LineWidth',4, 'LineStyle', '--')
+% end
 % highlighting edges for UGV
-highlight(GLNSPlot, S6, T6, 'EdgeColor', 'r', 'LineWidth', 4)
+% highlight(GLNSPlot, S6, T6, 'EdgeColor', 'r', 'LineWidth', 4)
 
-highlight(GLNSPlot, S2)             %highlights nodes
-highlight(GLNSPlot, numel(S2)+1)    %highlights last node
+% highlight(GLNSPlot, S2)             %highlights nodes
+% highlight(GLNSPlot, numel(S2)+1)    %highlights last node
 
---
+++ ICRA2018/noWhiteSpace.m
@@ -0,0 +1,16 @@
+
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
+
+fig = gcf;
+fig.PaperPositionMode = 'auto'
+fig_pos = fig.PaperPosition;
+fig.PaperSize = [fig_pos(3) fig_pos(4)];
+
+saveas(fig,'GLNS_compTime','epsc')
