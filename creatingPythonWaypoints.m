24245591c6e288d4e2ee70ff700003fb43d8f4b0
Kevin Li Yu <klyu@vt.edu>
2018-01-26 10:06:57 -0500

    used waypoints

--- creatingPythonWaypoints.m
+++ creatingPythonWaypoints.m
@@ -5,7 +5,7 @@ fileID = fopen(filename,'w');
 
 count = 1;
 for a = 1:numel(latlongele(:,1))
-    fprintf(fileID, 'Waypoint(frame = 3, command = 16, is_current = False, autocontinue = True, param1 = 5, x_lat = %f, y_long = %f, z_alt = 10),', latlongele(a,1),latlongele(a, 2));
+    fprintf(fileID, 'Waypoint(frame = 3, command = 16, is_current = 0, autocontinue = True, param1 = 5, x_lat = %f, y_long = %f, z_alt = 10),', latlongele(a,1),latlongele(a, 2));
 %     count = count+1;
 %     fprintf(fileID, '', latlongele(count));
 %     count = count+1;
