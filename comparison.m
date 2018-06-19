0c01181f2df6984e41b60a3806b21e4a18d265d3
Kevin Li Yu <klyu@vt.edu>
2018-06-19 18:15:02 -0400

    finds the total cost/need to make it depth first search for UGV paths

--- comparison.m
+++ comparison.m
@@ -20,7 +20,7 @@ highlight(p,minTree);
 plot(minTree, 'XData', x1, 'YData', y1);
 centers = [x1',y1'];
 radii = ones([numPointsInit, 1])*(budget/2);
-viscircles(centers, radii)
+% viscircles(centers, radii)
 tempSolution = pdist2(centers, centers);
 tempCircleHits = [];
 for i = 1:numPointsInit
@@ -46,6 +46,19 @@ while(min(min(isnan(tempCircleHits)))~=1)
     end
 end
 
+sitesCircleKeep = [sitesCircleKeep, circleKeep'];
+tempSitesCircleKeep = [];
+for i = 1:length(circleKeep)
+    testing = unique(sitesCircleKeep(i, :),'stable');
+    while length(testing)<numPointsInit+1
+        testing = [testing, NaN];
+    end
+    
+    tempSitesCircleKeep(end+1, :) = testing;
+    
+end
+sitesCircleKeep = tempSitesCircleKeep;
+
 keptCenters = zeros([length(circleKeep), 2]);
 for i = 1:length(circleKeep)
     keptCenters(i,1) = x1(circleKeep(i));
@@ -55,88 +68,126 @@ figure()
 plot(minTree, 'XData', x1, 'YData', y1);
 radii = ones([length(keptCenters), 1]) * (budget/2);
 viscircles(keptCenters, radii)
-1;
 
+totalUGVCostPath = 0;
+for i = 1:length(circleKeep)-1 % TODO: make this a depth first search instead of in order of circleKeep
+    [tempPath, tempCostUGVPath] = shortestpath(minTree, circleKeep(i), circleKeep(i+1));
+    totalUGVCostPath = totalUGVCostPath + tempCostUGVPath;
+end
 
-% TODO: run concorde on sitesCircleKeep for each row
-
-
-
-
-
-
-
-
-
-
-
-
-
-method = 0;
-numUGVs = 2;
-sitesPerUGV = numPointsInit/numUGVs;
-counter = 1;
-maxDistance = sqrt((area(1)-area(2))^2+(area(3)-area(4))^2)
+numUGVs = length(circleKeep);
+% sitesPerUGV = numPointsInit/numUGVs;
+% maxDistance = sqrt((area(1)-area(2))^2+(area(3)-area(4))^2);
+UGVsitesX = [];
+UGVsitesY = [];
 for i = 1:numUGVs
-    for j = 1:sitesPerUGV
-        UGVsitesX(i,j) = GLNSx(counter);
-        UGVsitesY(i,j) = GLNSy(counter);
-        counter = counter + 1;
+    for j = 1:numPointsInit+1
+        if isnan(sitesCircleKeep(i,j))
+            UGVsitesX(i,j) = NaN;
+            UGVsitesY(i,j) = NaN;
+        else
+            UGVsitesX(i,j) = x1(sitesCircleKeep(i,j));
+            UGVsitesY(i,j) = y1(sitesCircleKeep(i,j));
+        end
     end
 end
-UGVLocation = [];
-UGVLocation(:,end+1) = UGVsitesX(:, round(sitesPerUGV/2));
-UGVLocation(:,end+1) = UGVsitesY(:, round(sitesPerUGV/2));
-compCluster = 1:sitesPerUGV;
-solutions = nan(numUGVs, sitesPerUGV);
+UGVLocation = keptCenters;
+% UGVsitesX = [UGVsitesX, UGVLocation(:,1)];
+% UGVsitesY = [UGVsitesY, UGVLocation(:,2)];
+
+compCluster = 1:numPointsInit;
+solutions = nan(numUGVs, numPointsInit);
 totCosts = [];
 for k = 1: numUGVs
-    compG1 = graph([], []);
+    concordeG = graph([], []);
     tempX = UGVsitesX(k,:);
     tempY = UGVsitesY(k,:);
-    for i = 1:sitesPerUGV
-        for j = i+1:sitesPerUGV
+    tempX(isnan(tempX)) = [];
+    tempY(isnan(tempY)) = [];
+    for i = 1:length(tempX)
+        for j = i+1:length(tempX)
             points = [tempX(i), tempY(i); tempX(j), tempY(j)];
-            compG1 = addedge(compG1, i, j, pdist(points,'euclidean'));
+            tempWeight = pdist(points,'euclidean');
+            concordeG = addedge(concordeG, i, j, tempWeight);
+            
         end
     end
-    [Out_sol, ~] = TSP_tour_Dat(compG1,'/home/klyu/software/concorde/concorde/TSP/concorde');
-    while Out_sol(1) ~= round(sitesPerUGV/2)
-        Out_sol = circshift(Out_sol,1);
-        tempX = circshift(tempX,1);
-        tempY = circshift(tempY,1);
-    end
-    totDist = 0;
-    stops = 1;
-    extraDist = 0;
-    removalDist = [0];
-    for i = 1:sitesPerUGV-1
-        points = [tempX(i), tempY(i); tempX(i+1), tempY(i+1)];
-        dist = pdist(points,'euclidean');
-        totDist = totDist + dist;
-        if totDist > maxDistance*stops
-            removalDist(end+1) = dist;
-            stops = stops + 1;
-            points = [tempX(i), tempY(i); tempX(1), tempY(1)];
-            tempExtraDist = pdist(points, 'euclidean');
-            extraDist = extraDist + tempExtraDist;
-            points = [tempX(1), tempY(1); tempX(i+1), tempY(i+1)];
-            tempExtraDist = pdist(points, 'euclidean');
-            extraDist = extraDist + tempExtraDist;
-            i = i+1;
+    
+    if length(tempX)<3
+        Out_sol = sitesCircleKeep(k,:);
+        Out_sol(isnan(Out_sol)) = [];
+        if length(tempX) == 2
+            point = [x1(Out_sol(1)), y1(Out_sol(1)); x1(Out_sol(2)), y1(Out_sol(2))];
+            totDist = pdist(point, 'euclidean');
+        else % only one site
+            totDist = 0;
         end
+        while Out_sol(1) ~= circleKeep(k)
+            Out_sol = circshift(Out_sol,1);
+        end
+        totCosts(end+1) = 2*totDist + timeL + timeTO;
+        for i = length(tempX):numPointsInit-1
+            Out_sol = [Out_sol, NaN];
+        end
+        solutions(k,:) = Out_sol;
+        
+    else
+        [Out_sol, ~] = TSP_tour_Dat(concordeG,'/home/klyu/software/concorde/concorde/TSP/concorde');
+        while Out_sol(1) ~= circleKeep(k)
+            Out_sol = circshift(Out_sol,1);
+        end
+        totDist = 0;
+        stops = 1;
+        extraDist = 0;
+        removalDist = [0];
+        tempXNew = tempX;
+        tempYNew = tempY;
+        %     checker = sitesCircleKeep(k,:)
+        for i = 1:length(Out_sol)
+            %         tempCheck = Out_sol(i);
+            tempX(i) = tempXNew(Out_sol(i));
+            tempY(i) = tempYNew(Out_sol(i));
+        end
+        
+        for i = 1:length(Out_sol)-1
+            points = [tempX(i), tempY(i); tempX(i+1), tempY(i+1)];
+            dist = pdist(points,'euclidean');
+            totDist = totDist + dist;
+            if totDist > budget*stops
+                removalDist(end+1) = dist;
+                stops = stops + 1;
+                points = [tempX(i), tempY(i); tempX(1), tempY(1)];
+                tempExtraDist = pdist(points, 'euclidean');
+                extraDist = extraDist + tempExtraDist;
+                points = [tempX(1), tempY(1); tempX(i+1), tempY(i+1)];
+                tempExtraDist = pdist(points, 'euclidean');
+                extraDist = extraDist + tempExtraDist;
+                i = i+1;
+            end
+        end
+        points = [tempX(end), tempY(end); tempX(1), tempY(1)];
+        lastLeg = pdist(points, 'euclidean');
+        totCosts(end+1) = totDist - sum(removalDist) + extraDist + stops*(timeL+timeTO) + lastLeg;
+%         for i = length(tempX):numPointsInit-1
+%             Out_sol = [Out_sol; NaN];
+%         end
+        correctedOut_sol = nan([length(Out_sol),1]);
+        for i = 1:length(Out_sol)
+            correctedOut_sol(i) = sitesCircleKeep(k,Out_sol(i));
+        end
+        for i = length(tempX):numPointsInit-1
+            correctedOut_sol = [correctedOut_sol; NaN];
+        end
+        
+        solutions(k,:) = correctedOut_sol;
     end
-    points = [tempX(end), tempY(end); tempX(1), tempY(1)];
-    lastLeg = pdist(points, 'euclidean');
-    totCosts(end+1) = totDist - sum(removalDist) + extraDist + stops*(timeL+timeTO) + lastLeg;
-    solutions(k,:) = Out_sol;
 end
-UGVlegs = 0;
-for i = 1:numUGVs-1
-    points = [UGVLocation(i,1), UGVLocation(i,2); UGVLocation(i+1,1), UGVLocation(i+1,2)];
-    UGVlegs = UGVlegs + pdist(points, 'euclidean');
-end
-totCost = sum(totCosts) + UGVlegs;
+% UGVlegs = 0;
+% for i = 1:numUGVs-1
+%     points = [UGVLocation(i,1), UGVLocation(i,2); UGVLocation(i+1,1), UGVLocation(i+1,2)];
+%     UGVlegs = UGVlegs + pdist(points, 'euclidean');
+% end
+totCost = sum(totCosts) + totalUGVCostPath;
 
 % totCostMine = 0;
 % for i = 1:numPointsInit-1
